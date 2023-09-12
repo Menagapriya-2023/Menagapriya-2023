c44b7ab8faef5aa4815ea520d9ab970c7c337040
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-12 21:00:28 +0530

    Add files via upload
    Prime number

---
+++ IsPrime.java
@@ -0,0 +1,48 @@
+package week1.assignment;
+
+public class IsPrime {
+
+	public void isPrimeNumber(int num)
+	{
+		boolean flag=false;
+		//num Value should be greater one
+		if (num<=1)
+		{
+			flag=false;
+		}
+		
+		//iteration 
+		for(int i=2;i<=num/2;i++) {
+			// condition for prime number
+			if (num % i ==0) 	
+			{
+			flag=false;
+		}
+		}
+		if (!flag)
+		{
+			
+			System.out.println(num + "is a prime number");
+		}
+			
+		else {
+			System.out.println(num + " is not a prime number");
+		}
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// assigning num value to check the prime number
+		
+		IsPrime primeNum=new IsPrime();
+		primeNum.isPrimeNumber(38);
+		primeNum.isPrimeNumber(13);
+		
+	
+	}
+}
+
+	
+	
+	
+
+
