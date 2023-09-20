f258d9ba495bd828f9860f0392156e666e006643
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-20 16:01:27 +0530

    Add files via upload
    Day 2 Selenium assignments

---
+++ DeleteLead.java
@@ -0,0 +1,57 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DeleteLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver page = new ChromeDriver();
+		page.manage().window().maximize();
+		page.get("http://leaftaps.com/opentaps/control/main");
+		page.findElement(By.id("username")).sendKeys("demosalesmanager");
+		page.findElement(By.id("password")).sendKeys("crmsfa");
+		page.findElement(By.className("decorativeSubmit")).click();
+		page.findElement(By.linkText("CRM/SFA")).click();
+		page.findElement(By.linkText("Leads")).click();
+		page.findElement(By.linkText("Find Leads")).click();
+		page.findElement(By.linkText("Phone")).click();
+		//page.findElement(By.name("phoneCountryCode")).sendKeys("1");
+		//page.findElement(By.name("phoneAreaCode")).sendKeys("91");
+		page.findElement(By.name("phoneNumber")).sendKeys("99");
+		page.findElement(By.xpath("//button[contains(text(),'Find')]")).click();
+		Thread.sleep(1000);
+		page.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a")).click();
+		page.findElement(By.linkText("Delete")).click();
+		page.findElement(By.linkText("Find Leads")).click();
+		Thread.sleep(1000);
+		page.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a")).click();
+		page.findElement(By.linkText("Find Leads")).click();
+		page.findElement(By.name("id")).sendKeys("10059");
+		page.findElement(By.xpath("//button[contains(text(),'Find Leads')]")).click();
+		String s1="No records to display";
+		WebElement aa= page.findElement(By.xpath("//div[text()='No records to display']"));
+		  System.out.println(aa.getText());
+		   
+		  if(aa.equals(s1)) {
+			  System.out.println("No records to display");
+		   }else {
+		   System.out.println("LeadId is found");
+		  }
+		  //page.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
