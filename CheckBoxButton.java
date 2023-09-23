82f8d87fcb07a0e1b3680b57138701a9997b78d9
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-23 20:21:58 +0530

    Add files via upload
    CheckBoxButton Inheritance

---
+++ CheckBoxButton.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class CheckBoxButton extends RadioButton {
+	
+	public void clickCheckButton() {
+		
+		System.out.println("Check whether all are submitted");
+	
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
