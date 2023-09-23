2c1b7575e6499903a316e2cca46055082ad3a845
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-23 20:22:37 +0530

    Add files via upload
    Elements Inheritance

---
+++ Elements.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class Elements extends CheckBoxButton {
+	
+
+	public static void main(String[] args) {
+		
+		Elements ee=new Elements();
+		ee.click();
+		ee.setText("TestLeaf");
+		ee.submit();
+		ee.getText();
+		ee.clickCheckButton();
+		ee.selectRadioButton();
+		
+	}
+
+}
