4f4a24a79ec01e9366ecb002beb55ec1f6ded36e
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-23 20:18:53 +0530

    Add files via upload
    Inheritance

---
+++ WebElement.java
@@ -0,0 +1,21 @@
+package week3.day1;
+
+public class WebElement {
+	
+	public void  click()
+	{
+		System.out.println("Click it");
+	}
+	
+	public void  setText(String text) {
+		
+		System.out.println(text);
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
