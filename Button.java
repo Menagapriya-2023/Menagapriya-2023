1d7d3ef96f2e0d83eb4eb6cd3523a9a884f58f3d
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-23 20:20:32 +0530

    Add files via upload
    Button inheritance

---
+++ Button.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class Button extends TextField {
+	
+	public void submit() 
+	{
+		System.out.println("submit home assignments on time");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
