5f39e0e5f8a6af3214e0c269e233fc49634e9bec
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-23 20:23:38 +0530

    Add files via upload
    TestData single inheritance

---
+++ TestData.java
@@ -0,0 +1,23 @@
+package week3.day1;
+
+public class TestData {
+	
+	String name ="Menagapriya";
+	String password ="Test@123";
+
+	public void enterCredentials()
+	{
+	System.out.println("name is " +name);	
+	}
+	public void navigateToHomePage() 
+	{
+		System.out.println("password is " +password);	
+	}
+	public static void main(String[] args) {
+		
+	
+		
+
+	}
+
+}
