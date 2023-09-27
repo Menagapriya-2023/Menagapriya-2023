8ce03aa8f6063dccf4baccfdcd62434b07ea8774
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-27 14:56:08 +0530

    Add files via upload
    Interface2

---
+++ DatabaseConnection.java
@@ -0,0 +1,14 @@
+package week3.day2.Assignments;
+
+public interface DatabaseConnection {
+	
+	public abstract void connect();
+	
+	public abstract void disconnect();
+	
+	
+	
+	public abstract void executeUpdate();
+
+
+}
