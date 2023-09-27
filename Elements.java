ee81105162cae1b325ac0c0af2fd5c18676dbff6
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-27 09:59:20 +0530

    Add files via upload
    Week3 day1 interface

--- Elements.java
+++ Elements.java
@@ -1,17 +1,21 @@
 package week3.day1;
 
-public class Elements extends CheckBoxButton {
+public class Elements extends Button {
+	
 	
 
 	public static void main(String[] args) {
 		
+		TextField tf=new TextField();
+		RadioButton rb=new RadioButton();
+		CheckBoxButton cb=new CheckBoxButton();
 		Elements ee=new Elements();
 		ee.click();
 		ee.setText("TestLeaf");
 		ee.submit();
-		ee.getText();
-		ee.clickCheckButton();
-		ee.selectRadioButton();
+	    tf.getText();
+		cb.clickCheckButton();
+		rb.selectRadioButton();
 		
 	}
 
