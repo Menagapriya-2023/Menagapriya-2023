7f28027eda8204e8ae1649a77a22104103dff792
Menagapriya-2023 <144817316+Menagapriya-2023@users.noreply.github.com>
2023-09-28 12:37:58 +0530

    Add files via upload
    Intersection using list concept

--- FindIntersection.java
+++ FindIntersection.java
@@ -1,24 +1,36 @@
 package week3.day2.Assignments;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class FindIntersection {
 
 	public static void main(String[] args) {
 		int [] arr1 = {3, 2, 11, 4, 6, 7};  //declaring the Array
 		int arr2 []= {1, 2, 8, 4, 9, 7};    //Declaring the Array
 		
-		for (int i=0;i<arr1.length;i++)
+		//declare the two linkedlist to store the array values
+		List<Integer> input1=new LinkedList<Integer> ();
+		List<Integer> input2=new LinkedList<Integer> ();
+		
+	//convert the arr1 values into input1	
+		for (int num :arr1)
+			{
+			input1.add(num);
+		}
+		
+		////convert the arr1 values into input2
+		for (int num :arr2)
 		{
-			for (int j=0;j<arr2.length;j++)
-			{
-				
-			if(arr1[i]==arr2[j])
-			{
-				System.out.println("Intersection numbers are "+arr2[j]);
-				
-			}
+		input2.add(num);
+	}
+		//declare one more linkedlist and add input1
+		List<Integer> Intersection=new LinkedList<Integer> (input1);
+		Intersection.retainAll(input2);
+		System.out.println(Intersection);
+	
 		}
-		}
+		
 	
 		
 	}
-}
